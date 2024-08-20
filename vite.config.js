6e1f8ab0ec50614da7b912052a93213177ec0237
Goutham V <gouthamganesh.v@genisys-group.com>
2024-08-20 11:04:08 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'public'
-}
-})
+  base: 'farmers-market', // Replace with your repo name
+});
